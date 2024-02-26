e5f73ebc67767f2620244c9624f28d3c54a5b9f9
Alexandre Herculano <aherculano0109@gmail.com>
2024-02-26 14:31:36 +0000

    new migrations added

---
+++ src/Migrations/Migrations/20240226125617_AddMissingReviewProperties.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissingReviewProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ReviewDate",
+                table: "BookReview",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UniqueIdentifier",
+                table: "BookReview",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReviewDate",
+                table: "BookReview");
+
+            migrationBuilder.DropColumn(
+                name: "UniqueIdentifier",
+                table: "BookReview");
+        }
+    }
+}
