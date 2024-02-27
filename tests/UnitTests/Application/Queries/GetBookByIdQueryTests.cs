fc3a9c049da49988f984c13b98890d14219832a3
Alexandre Herculano <aherculano0109@gmail.com>
2024-02-27 13:02:20 +0000

    added unit tests for GetBookByIdQuery.cs

---
+++ tests/UnitTests/Application/Queries/GetBookByIdQueryTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Application.Queries;
+using AutoFixture;
+using Domain.Interfaces;
+using Domain.Models;
+using FluentAssertions;
+using FluentResults;
+using NSubstitute;
+using Xunit;
+
+namespace UnitTests.Application.Queries;
+
+[ExcludeFromCodeCoverage]
+public class GetBookByIdQueryTests: TestsBase
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly GetBookByIdQueryHandler _handler;
+    
+    public GetBookByIdQueryTests()
+    {
+        _unitOfWork = Fixture.Freeze<IUnitOfWork>();
+        _handler = new GetBookByIdQueryHandler(_unitOfWork);
+    }
+
+    [Fact]
+    public async void Handle_RepositoryReturnsFail_ReturnFail()
+    {
+        //Arrange
+        var query = Fixture.Create<GetBookByIdQuery>();
+        
+        _unitOfWork.BookRepository
+            .GetBookAsync(Fixture.Create<Guid>())
+            .Returns(Result.Fail(Fixture.Create<Error>()));
+
+        //Act
+        var result = await _handler.Handle(query, CancellationToken.None);
+
+        //Assert
+        result.IsFailed.Should().BeTrue();
+        await _unitOfWork.BookRepository.Received(1).GetBookAsync(Arg.Any<Guid>());
+    }
+
+    [Fact]
+    public async void Handle_RepositoryReturnsOk_ResultOk()
+    {
+        //Arrange
+        var query = Fixture.Create<GetBookByIdQuery>();
+        var book = Fixture.Build<Book>()
+            .With(x => x.UniqueIdentifier, query.BookUniqueIdentifier)
+            .Create();
+        _unitOfWork.BookRepository.GetBookAsync(query.BookUniqueIdentifier).Returns(Result.Ok(book));
+
+        //Act
+        var result = await _handler.Handle(query, CancellationToken.None);
+
+        //Assert
+        result.IsSuccess.Should().BeTrue();
+        await _unitOfWork.BookRepository.Received(1).GetBookAsync(query.BookUniqueIdentifier);
+    }
+    
+}
